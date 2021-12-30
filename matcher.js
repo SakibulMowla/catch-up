74bb6d9eebbc99ff9e4b020ecebf3d4716bb93e0
Sakibul Mowla <sakibulmowla@gmail.com>
2021-12-30 20:05:09 +0000

    Add matcher class to figure the email groups

---
+++ matcher.js
@@ -0,0 +1,93 @@
+const AWS = require('aws-sdk');
+const DBWrapper = require('./dbwrapper');
+
+AWS.config.update({
+  region: 'us-west-2',
+  endpoint: 'https://dynamodb.us-west-2.amazonaws.com',
+});
+
+const MeetingTable = 'catchupmeetings';
+const DevPrefix = 'dev';
+
+class Matcher {
+  constructor(orderedAllUserList) {
+    this.dbWrapper = new DBWrapper();
+    this.dynamodb = new AWS.DynamoDB();
+    this.docClient = new AWS.DynamoDB.DocumentClient();
+    this.assignedUsers = new Set();
+    this.orderedAllUserList = orderedAllUserList;
+    this.totalUsers = this.orderedAllUserList.length;
+  }
+
+  async getPreferenceList(user, tier = 'dev') {
+    const params = {
+      ExpressionAttributeValues: {
+        ':email1': { S: user.email },
+      },
+      KeyConditionExpression: 'email1 = :email1',
+      ProjectionExpression: 'email2, #timestamp',
+      ExpressionAttributeNames: {
+        '#timestamp': 'timestamp',
+      },
+      TableName: (tier === 'dev' ? DevPrefix : '') + MeetingTable,
+      ScanIndexForward: false,
+      Limit: this.totalUsers - 1,
+    };
+
+    const response = await this.dbWrapper.query(params);
+    console.log('getPreferenceList response = ', JSON.stringify(response, null, 2));
+
+    const preferenceList = [];
+    const preferenceSet = new Set();
+    response.Items.forEach((element) => {
+      if (preferenceSet.has(element.email2.S)) {
+        return;
+      }
+
+      preferenceList.unshift(element.email2.S);
+      preferenceSet.add(element.email2.S);
+    });
+
+    this.orderedAllUserList.forEach((element) => {
+      if (!preferenceSet.has(element.email) && element.email !== user.email) {
+        preferenceList.unshift(element.email);
+      }
+    });
+
+    console.log('preferenceList response = ', JSON.stringify(preferenceList, null, 2));
+
+    return preferenceList;
+  }
+
+  async getMatchedGroups(tier = 'dev') {
+    const matchedGroups = [];
+    // 1. for...of is needed here to send the getPreferenceList calls in sequence
+    // 2. using map makes it parallel and doesn't preserve the order of the list
+    // 3. forEach can't be used when await is inside
+    // eslint-disable-next-line no-restricted-syntax
+    for (const userA of this.orderedAllUserList) {
+      if (this.assignedUsers.has(userA.email)) {
+        break;
+      }
+
+      // eslint-disable-next-line no-await-in-loop
+      const preferenceList = await this.getPreferenceList(userA, tier);
+      const userBemail = preferenceList.find((email) => !this.assignedUsers.has(email));
+      console.log('userA = ', JSON.stringify(userA, null, 2));
+      console.log('userBemail = ', userBemail);
+      if (userBemail) {
+        console.log('userA.email = ', userA.email);
+        console.log('userBemail = ', userBemail);
+        matchedGroups.push([userA.email, userBemail]);
+        this.assignedUsers.add(userA.email);
+        this.assignedUsers.add(userBemail);
+      }
+    }
+
+    console.log('matchedGroups = ', JSON.stringify(matchedGroups, null, 2));
+
+    return matchedGroups;
+  }
+}
+
+module.exports = Matcher;
---
+++ matcher.test.js
@@ -0,0 +1,192 @@
+const DBWrapper = require('./dbwrapper');
+const delay = require('./library');
+const Matcher = require('./matcher');
+const UserPrioritizer = require('./userprioritizer');
+
+const dbWrapper = new DBWrapper();
+const userPrioritizer = new UserPrioritizer();
+
+const UserTable = 'devcatchupusers';
+const MeetingTable = 'devcatchupmeetings';
+
+beforeAll(async () => {
+  const userTableParams = {
+    TableName: UserTable,
+    KeySchema: [
+      { AttributeName: 'email', KeyType: 'HASH' }, // Partition key
+    ],
+    AttributeDefinitions: [
+      { AttributeName: 'email', AttributeType: 'S' },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 10,
+      WriteCapacityUnits: 10,
+    },
+  };
+  await dbWrapper.createTable(userTableParams);
+
+  const meetingTableParams = {
+    TableName: MeetingTable,
+    KeySchema: [
+      { AttributeName: 'email1', KeyType: 'HASH' }, // Partition key
+      { AttributeName: 'timestamp', KeyType: 'RANGE' }, // //Sort key
+    ],
+    AttributeDefinitions: [
+      { AttributeName: 'email1', AttributeType: 'S' },
+      { AttributeName: 'timestamp', AttributeType: 'S' },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 10,
+      WriteCapacityUnits: 10,
+    },
+  };
+  await dbWrapper.createTable(meetingTableParams);
+
+  // wait for db setup to finish
+  await delay(60 * 1000);
+
+  const batchWriteParams = {
+    RequestItems: {
+      devcatchupusers: [
+        {
+          PutRequest: {
+            Item: {
+              KEY: { S: 'email' },
+              email: { S: 'sakibulmowla@gmail.com' },
+              firstname: { S: 'Sakibul' },
+              lastname: { S: 'Mowla' },
+            },
+          },
+        },
+        {
+          PutRequest: {
+            Item: {
+              KEY: { S: 'email' },
+              email: { S: 'masum.nayeem@gmail.com' },
+              firstname: { S: 'Kazi' },
+              lastname: { S: 'Nayeem' },
+            },
+          },
+        },
+        {
+          PutRequest: {
+            Item: {
+              KEY: { S: 'email' },
+              email: { S: 'biswajit.sust@gmail.com' },
+              firstname: { S: 'Biswajit' },
+              lastname: { S: 'Debnath' },
+            },
+          },
+        },
+      ],
+    },
+  };
+  await dbWrapper.batchWriteItem(batchWriteParams);
+}, 5000 + 60 * 1000);
+
+afterAll(async () => {
+  const userTableParams = {
+    TableName: UserTable,
+  };
+  await dbWrapper.deleteTable(userTableParams);
+
+  const meetingTableParams = {
+    TableName: MeetingTable,
+  };
+  await dbWrapper.deleteTable(meetingTableParams);
+
+  // wait for db deletion to finish
+  await delay(60 * 1000);
+}, 5000 + 60 * 1000);
+
+test('xxxxxxxs', async () => {
+  const dec31st2021DateString = new Date(2021, 12, 31).toISOString();
+  const dec21st2021DateString = new Date(2021, 12, 21).toISOString();
+
+  const batchWriteParamsForPutting = {
+    RequestItems: {
+      devcatchupmeetings: [
+        {
+          PutRequest: {
+            Item: {
+              KEY: { S: 'email1' },
+              email1: { S: 'masum.nayeem@gmail.com' },
+              email2: { S: 'biswajit.sust@gmail.com' },
+              timestamp: { S: dec31st2021DateString },
+            },
+          },
+        },
+        {
+          PutRequest: {
+            Item: {
+              KEY: { S: 'email1' },
+              email1: { S: 'biswajit.sust@gmail.com' },
+              email2: { S: 'masum.nayeem@gmail.com' },
+              timestamp: { S: dec31st2021DateString },
+            },
+          },
+        },
+        {
+          PutRequest: {
+            Item: {
+              KEY: { S: 'email1' },
+              email1: { S: 'sakibulmowla@gmail.com' },
+              email2: { S: 'biswajit.sust@gmail.com' },
+              timestamp: { S: dec21st2021DateString },
+            },
+          },
+        },
+        {
+          PutRequest: {
+            Item: {
+              KEY: { S: 'email1' },
+              email1: { S: 'biswajit.sust@gmail.com' },
+              email2: { S: 'sakibulmowla@gmail.com' },
+              timestamp: { S: dec21st2021DateString },
+            },
+          },
+        },
+      ],
+    },
+  };
+  await dbWrapper.batchWriteItem(batchWriteParamsForPutting);
+
+  const orderedUserList = await userPrioritizer.getOrderedListOfUsers('dev');
+  const matcher = new Matcher(orderedUserList);
+
+  const preferenceListOfSakibul = await matcher.getPreferenceList(
+    {
+      firstname: 'Sakibul',
+      lastname: 'Mowla',
+      email: 'sakibulmowla@gmail.com',
+      timestamp: dec21st2021DateString,
+    },
+    'dev',
+  );
+  expect(preferenceListOfSakibul).toStrictEqual([
+    'masum.nayeem@gmail.com',
+    'biswajit.sust@gmail.com',
+  ]);
+
+  const preferenceListOfBiswajit = await matcher.getPreferenceList(
+    {
+      firstname: 'Biswajit',
+      lastname: 'Debnath',
+      email: 'biswajit.sust@gmail.com',
+      timestamp: dec31st2021DateString,
+    },
+    'dev',
+  );
+  expect(preferenceListOfBiswajit).toStrictEqual([
+    'sakibulmowla@gmail.com',
+    'masum.nayeem@gmail.com',
+  ]);
+
+  const matchedGroups = await matcher.getMatchedGroups('dev');
+  expect(matchedGroups).toStrictEqual([
+    [
+      'sakibulmowla@gmail.com',
+      'masum.nayeem@gmail.com',
+    ],
+  ]);
+});
