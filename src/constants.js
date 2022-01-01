c475a45f828e720b98b4c31f2193100b8184973d
Sakibul Mowla <sakibulmowla@gmail.com>
2022-01-01 01:35:08 +0000

    Add admin constants

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,8 @@
 module.exports = {
+  ADMIN: {
+    EMAIL: 'sakibulmowla@gmail.com',
+    FULLNAME: 'Catchup Admin',
+  },
   DB_SETUP_WAIT_TIME_MS: 10 * 1000,
   JEST_DEFAULT_TIMEOUT_MS: 5000,
   TABLE: {
